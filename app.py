62aa25a7acde1c57d26821514e18e8ef560c5283
Kishore <stonebench04@gmail.com>
2026-04-18 00:28:03 +0530

    IT Helpdesk Troubleshooting Assistant

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import datetime
+import os
+
+app = Flask(__name__)
+
+def log_issue(issue):
+    with open("issues_log.txt", "a") as file:
+        time = datetime.datetime.now()
+        file.write(f"{time} - {issue}\n")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    solution = ""
+
+    if request.method == "POST":
+        issue = request.form["issue"]
+
+        if issue == "internet":
+            solution = "Check WiFi, restart router, run troubleshooter"
+            log_issue("Internet Issue")
+
+        elif issue == "slow":
+            solution = "Close apps, restart system, check disk space"
+            log_issue("Slow System")
+
+        elif issue == "software":
+            solution = "Restart software, reinstall, check updates"
+            log_issue("Software Issue")
+
+        elif issue == "printer":
+            solution = "Check cable/WiFi, restart printer, reinstall driver"
+            log_issue("Printer Issue")
+
+    return render_template("index.html", solution=solution)
+
+if __name__ == "__main__":
+   port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)

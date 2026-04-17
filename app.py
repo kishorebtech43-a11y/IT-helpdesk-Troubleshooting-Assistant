b1b299c6aaf7c45e215a1d027f424525255b4484
Kishore <stonebench04@gmail.com>
2026-04-18 01:09:03 +0530

    fix indentation error

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import datetime
-import os
 
 app = Flask(__name__)
 
@@ -34,6 +33,3 @@ def home():
 
     return render_template("index.html", solution=solution)
 
-if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
